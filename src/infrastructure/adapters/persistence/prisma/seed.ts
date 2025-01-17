922ff408440716fabcc67214197b69a0d2ae0379
David Basto <davidbasto01@gmail.com>
2025-01-17 08:03:24 -0500

    update: seed images urls

--- src/infrastructure/adapters/persistence/prisma/seed.ts
+++ src/infrastructure/adapters/persistence/prisma/seed.ts
@@ -13,7 +13,7 @@ async function main() {
         'The Great Gatsby is a novel by American writer F. Scott Fitzgerald.',
       price: 50000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/61EtTpQI3vL._SL1360_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/61EtTpQI3vL._SL300_.jpg',
     },
     {
       title: 'To Kill a Mockingbird',
@@ -22,7 +22,7 @@ async function main() {
         'To Kill a Mockingbird is a novel by Harper Lee published in 1960.',
       price: 45000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/51tDHl8Z7cL.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/51tDHl8Z7cL._SL300_.jpg',
     },
     {
       title: 'Enamórate de ti',
@@ -31,7 +31,7 @@ async function main() {
         'Desde pequeños nos enseñan conductas de cuidado personal respecto al físico: lavarnos los dientes, bañarnos, arreglarnos las uñas, comer, aprender a vestirnos...',
       price: 45000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/719fMYMMSiL._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/719fMYMMSiL._SL300_.jpg',
     },
     {
       title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
@@ -40,7 +40,7 @@ async function main() {
         'Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees.',
       price: 145000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/81KpjloJJZL._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/81KpjloJJZL._SL300_.jpg',
     },
     {
       title: 'The Pragmatic Programmer',
@@ -49,7 +49,7 @@ async function main() {
         'The Pragmatic Programmer is one of those rare tech audiobooks you’ll listen, re-listen, and listen to again over the years.',
       price: 95000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/91WFb-PpoNL._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/91WFb-PpoNL._SL300_.jpg',
     },
     {
       title: 'El Alquimista',
@@ -58,7 +58,7 @@ async function main() {
         'TCuando una persona desea realmente algo, el Universo entero conspira para que pueda realizar su sueño. Basta con aprender a escuchar los dictados del corazón y a descifrar el lenguaje que está más allá de las palabras.',
       price: 75000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/71uljqFndcL._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/71uljqFndcL._SL300_.jpg',
     },
     {
       title: 'Cien años de soledad (50 Aniversario)',
@@ -67,7 +67,7 @@ async function main() {
         'En ocasión del 50 aniversario de la publicación de Cien años de soledad, llega una edición con ilustraciones inéditas de la artista chilena Luisa Rivera y con una tipografía creada por el hijo del autor, Gonzalo García Barcha.',
       price: 99000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/81QvZFjaI+L._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/81QvZFjaI+L._SL300_.jpg',
     },
     {
       title: 'El amor en los tiempos del cólera (Edición ilustrada)',
@@ -76,7 +76,7 @@ async function main() {
         'Una edición conmemorativa de El amor en los tiempos del cólera, un gran clásico de Gabriel García Márquez y una novela imprescindible de la literatura contemporánea.',
       price: 99000,
       stock: 100,
-      image_url: 'https://m.media-amazon.com/images/I/91IxbReQwhL._SL1500_.jpg',
+      image_url: 'https://m.media-amazon.com/images/I/91IxbReQwhL._SL300_.jpg',
     },
   ];
 
